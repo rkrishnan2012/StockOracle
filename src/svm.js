894fdbafc7a7b6341be5797abc55bad98f7c9aef
Rohit Krishnan <tanjil_mustafa@student.uml.edu>
2017-01-03 19:32:04 +0530

    Ayee got some gains :)

--- src/svm.js
+++ src/svm.js
@@ -1,5 +1,6 @@
 const svm = require('node-svm');
 const so = require('stringify-object');
+const ProgressBar = require('progress');
 
 module.exports.analyzeData = function(dailyIndicatorList) {
     var training = [];
@@ -15,7 +16,7 @@ module.exports.analyzeData = function(dailyIndicatorList) {
             testing.push([]);
             symbols.push(symbol);
             names.push(dailyIndicatorList[symbol].name);
-            for (var dayNumber = 0; dayNumber < (2 * dailyIndicatorList[symbol].indicators.length) / 3; dayNumber++) {
+            for (var dayNumber = 0; dayNumber < dailyIndicatorList[symbol].indicators.length - 30; dayNumber++) {
                 var xi = [];
                 for (var indicator in dailyIndicatorList[symbol].indicators[dayNumber]) {
                     xi.push(dailyIndicatorList[symbol].indicators[dayNumber][indicator]);
@@ -23,7 +24,7 @@ module.exports.analyzeData = function(dailyIndicatorList) {
                 training[i].push([xi, (dailyIndicatorList[symbol].indicators[dayNumber + 1].close - dailyIndicatorList[symbol].indicators[dayNumber].close) > 0 ? 1 : 0]);
             }
 
-            for (var dayNumber = Math.round((2 * dailyIndicatorList[symbol].indicators.length) / 3); dayNumber < dailyIndicatorList[symbol].indicators.length - 1; dayNumber++) {
+            for (var dayNumber = dailyIndicatorList[symbol].indicators.length - 30; dayNumber < dailyIndicatorList[symbol].indicators.length - 1; dayNumber++) {
                 var xi = [];
                 for (var indicator in dailyIndicatorList[symbol].indicators[dayNumber]) {
                     xi.push(dailyIndicatorList[symbol].indicators[dayNumber][indicator]);
@@ -41,14 +42,19 @@ module.exports.analyzeData = function(dailyIndicatorList) {
     var highestIdx = 0;
     var bestModel;
 
+    var bar = new ProgressBar('Training - :percent :bar :eta seconds remaining', {
+        total: training.length
+    });
+
     function trainNext() {
+        bar.tick();
         var clf = new svm.SVM({
             svmType: 'C_SVC',
-            c: 50,
+            c: [10, 20, 30],
 
             // kernels parameters 
             kernelType: 'RBF',
-            gamma: 3,
+            gamma: [1, 3, 5, 7],
 
             // training options 
             kFold: 2,
@@ -61,7 +67,7 @@ module.exports.analyzeData = function(dailyIndicatorList) {
             probability: false
         });
         clf.train(training[i]).progress(function(progress) {
-            //console.log('training progress: %d%', Math.round(progress * 100));
+
         }).spread(function(model, report) {
             if (report.fscore > highestFScore) {
                 highestFScore = report.fscore;
@@ -69,11 +75,11 @@ module.exports.analyzeData = function(dailyIndicatorList) {
                 highestReport = report;
                 highestIdx = i;
                 bestModel = model;
-                console.log("Current highest fscore is " + highestFScore + " by " + highestSymbol);
+                //console.log("Current highest fscore is " + highestFScore + " by " + highestSymbol);
             }
             i++;
             if (i == training.length) {
-                console.log("Done training! Highest fscore is " + highestFScore + " by " + highestSymbol);
+                console.log("Done training against " + i + " companies. Highest fscore is " + highestFScore + " by " + highestSymbol);
                 console.log(highestReport);
                 predictGains(svm, bestModel, names[highestIdx], testing[highestIdx]);
             } else {
@@ -91,23 +97,31 @@ function predictGains(svm, model, name, testSet) {
     var clf = svm.restore(model);
     var money = 1000;
     var shares = 0;
+    var bought = 0;
+    var sold = 0;
     for (var day = 0; day < testSet.length - 1; day++) {
         //	Tomorrow stocks will go up babyyyy
         var p = clf.predictSync(testSet[day][0]);
-        //console.log(p);
         if (p == 1) {
-            //console.log("Buying at " + testSet[day][1]);
-            while(money > 3 * testSet[day][1]) {
-            	money -= 3 * testSet[day][1];
-            	shares += 3;
+            while (money > 3 * testSet[day][1]) {
+                money -= 3 * testSet[day][1];
+                shares += 3;
+                bought++;
             }
         } else {
-            while(shares > 3) {
-            	//console.log("Selling at " + testSet[day+1][1]);
-            	money += 3 * testSet[day+1][1];
-            	shares-=3;
+            while (shares > 3) {
+                money += 3 * testSet[day + 1][1];
+                shares -= 3;
+                sold++;
             }
         }
     }
-    console.log("Left over money after " + testSet.length + " days is $" + money)
+
+    //	Sell whatever is at the end.
+    while (shares > 0) {
+        money += testSet[testSet.length - 1][1];
+        shares --;
+        sold++;
+    }
+    console.log("Left over money after " + testSet.length + " days is $" + money + ". Bought " + bought + " shares and sold " + sold + ".");
 }
